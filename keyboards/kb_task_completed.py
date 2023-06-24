9c15ed0cd358cf1732fec7edfc40d66cb75fd646
Iskhakov Aidar <137269246+SadOnsGit@users.noreply.github.com>
2023-06-24 06:43:52 +0300

    Delete kb_task_completed.py

--- keyboards/kb_task_completed.py
+++
@@ -1,4 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-kb_completed_task = InlineKeyboardMarkup()
-btn_completed = InlineKeyboardButton("Я оформил ✅", callback_data=)
