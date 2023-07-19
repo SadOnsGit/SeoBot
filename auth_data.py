701b936e435ce9bd54fdcbf00d2e8d74d5802393
Iskhakov Aidar <137269246+SadOnsGit@users.noreply.github.com>
2023-07-19 11:45:55 +0300

    Update auth_data.py

--- auth_data.py
+++ auth_data.py
@@ -1,16 +1,16 @@
 # Данные бота
-TOKEN = '5712488845:AAEBjE92cT23JoPOLgw0TqT-X65t4emTZ2E'  # Токен бота
+TOKEN = ''  # Токен бота
 # Платёжка кристал пей - данные
-CRYSTAL_NAME = 'sadons'  # Название кассы
-CRYSTAL_SECRET1 = '27e75a92095c0747871eb7e8e4a0023d2fd83d83'  # Секретный ключ №1
+CRYSTAL_NAME = ''  # Название кассы
+CRYSTAL_SECRET1 = ''  # Секретный ключ №1
 # Иды админов
-ADMINS = ['1112323335', '22814883132']
+ADMINS = ['', '']
 # Иды чатов
-SEO_CHAT_ID = '-1001775453891'  # Заявки на СЕО
-ZALIV_CHAT_ID = '-1001816424693'  # Заявки на ЗАЛИВ
-QUEUEINFO_CHAT_ID = '-1001858470181'  # Группа для постинга мест в очереди
+SEO_CHAT_ID = '-'  # Заявки на СЕО
+ZALIV_CHAT_ID = '-'  # Заявки на ЗАЛИВ
+QUEUEINFO_CHAT_ID = '-'  # Группа для постинга мест в очереди
 # ДАННЫЕ ДЛЯ ВХОДА POSTGRESQL
 HOST = '127.0.0.1'  # localhost
 USER = 'postgres'  # username
-PASSWORD = 'acab00acab'  # password
+PASSWORD = 'password'  # password
 DB_NAME = 'seobot_db'  # database name
